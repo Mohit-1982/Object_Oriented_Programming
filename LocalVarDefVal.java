288f684004724a350227d5cb15130d50048532a4
Mohit <mohitdagar2k08@gmail.com>
2026-04-19 10:30:53 +0530

    Create LocalVariableDefaultValue class
    Added a class to demonstrate local variable default values in Java.

---
+++ LocalVarDefVal.java
@@ -0,0 +1,10 @@
+package OOPS;
+
+public class LocalVariableDefaultValue {
+    public static void main(String[] args) {
+        int x;//this print statement will show error
+        //System.out.println(x);//Local Variable doesn't have default value
+        Integer y;
+        //System.out.println(y);even wrapper classes needs initialization
+    }
+}
