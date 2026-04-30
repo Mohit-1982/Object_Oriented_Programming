5ae52049d2a1f81db92e230028dcf8140590a750
Mohit <mohitdagar2k08@gmail.com>
2026-04-30 18:05:39 +0530

    Create StringMethods.java

---
+++ StringMethods.java
@@ -0,0 +1,68 @@
+package OOPS.Day10;
+
+import Recursion.SticklerThief;
+
+import java.util.Arrays;
+
+public class StringMethods {
+    public static void main(String[] args) {
+//        String s = new String("   ");
+//        System.out.println(s.length());//4
+//        System.out.println(s.isEmpty());//false
+//        System.out.println(s.isBlank());//true
+
+//        String str = "Mohit";
+//        System.out.println(str.charAt(3));
+//        char[] arr = str.toCharArray();//convert the string into a character array
+//        System.out.println(arr);
+
+//        String s = "Mohit";
+//        String str = "MOHIT";
+//        String btr = new String("Mohit");
+//        System.out.println(btr == str);
+//        System.out.println(s.equals(str));//equals check value in string --> it overrides this method (real one do this == obj internally)
+//        System.out.println(s.equalsIgnoreCase(str));//this compares after ignoring cases (capital and small letters)
+
+        String s = "Mohit";
+        String z = "Rohit";
+//        System.out.println(s.compareTo(z));//this will compare two strings and gives -ve, +ve or 0
+//
+//        System.out.println(s.contains("ohit"));//it checks s contains string "ohit" in it or not
+//        System.out.println(s.indexOf("i"));
+//        System.out.println(s.indexOf("it"));//returns starting point of this string
+//        System.out.println(s.startsWith("M"));
+//        System.out.println(s.lastIndexOf("i"));
+
+//        System.out.println(s.substring(1, 4));
+//        System.out.println(s.toLowerCase());
+//        System.out.println(s.toUpperCase());
+
+//        String st = "       Mohit     ";
+//        System.out.println(st.trim());
+
+//        System.out.println(s.trim());
+//        System.out.println(s.strip());
+//        System.out.println(s.repeat(3));
+
+//        System.out.println(s.replace("i", "m"));
+//        System.out.println(s.replace("it", "Mot"));
+//        System.out.println(s.replaceAll("it", "Mohit"));//this will replace every occu of it with Mohit in string s
+
+//        String s2 = "Mohit-Aditya-Rohit";
+//        String[] arr = s2.split("-");
+//        System.out.println(Arrays.toString(arr));
+
+//        System.out.println(String.join(", ", "a", "b", "b", "c"));
+
+//        String s4 = new String(String.valueOf(10.01));//converts int float double to string
+//        System.out.println(s4);
+
+//        String str = "MOHIT][=-0987654321`~!@#$%^&*()_+|}{:;[]"; we can find ascii values of character by converting them in byte array using getBytes() method
+//        System.out.println(Arrays.toString(str.getBytes()));
+
+//        String name = "Mohit";
+//        int age = 28;
+//
+//        System.out.println(String.format("Hello %s, Your age is : %s", name, age));
+    }
+}
